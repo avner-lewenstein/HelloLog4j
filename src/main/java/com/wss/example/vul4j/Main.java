be4033d1357c65b3612d20325ad8e5e8fda01c4b
avnerlewenstein <avner.lewenstein@whitesourcesoftware.com>
2022-11-14 10:11:35 +0200

    Initial commit

---
+++ src/main/java/com/wss/example/vul4j/Main.java
@@ -0,0 +1,12 @@
+package com.wss.example.vul4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world");
+    }
+}
