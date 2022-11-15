b36830706cbf8eb883dfeab60b644d0b48c2e93a
avnerlewenstein <avner.lewenstein@whitesourcesoftware.com>
2022-11-15 16:04:41 +0200

    Fix repoID + add vulnerable json.org:json library and usage of.

--- src/main/java/com/wss/example/vul4j/Main.java
+++ src/main/java/com/wss/example/vul4j/Main.java
@@ -2,11 +2,16 @@ package com.wss.example.vul4j;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 
 public class Main {
     private static Logger logger = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         logger.info("Hello world");
+
+        final JSONObject jsonObject = new JSONObject("{\"x\": \"y\", \"z\": [1, 2, 3]}");
+        System.out.println(jsonObject.getJSONArray("z").length());
+        Thread.sleep(60000);
     }
 }
