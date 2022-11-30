80f68ee66b8ab815fd79aaeadf0c036b96e934e5
avnerlewenstein <avner.lewenstein@whitesourcesoftware.com>
2022-11-30 13:50:53 +0200

    Use vulnerable jackson

--- src/main/java/com/wss/example/vul4j/Main.java
+++ src/main/java/com/wss/example/vul4j/Main.java
@@ -1,18 +1,24 @@
 package com.wss.example.vul4j;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Map;
 
 public class Main {
     private static Logger logger = LogManager.getLogger(Main.class);
+    private static ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         for (int i= 0; i < 100; i++) {
             logger.info("Hello world");
 
-            final JSONObject jsonObject = new JSONObject("{\"x\": \"y\", \"z\": [1, 2, 3]}");
-            System.out.println(jsonObject.getJSONArray("z").length());
+
+            final X x = mapper.readValue(new StringReader("{\"x\": \"y\", \"z\": [1, 2, 3]}"), X.class);
+            logger.info(mapper.writeValueAsString(x));
             Thread.sleep(5000);
         }
     }
---
+++ src/main/java/com/wss/example/vul4j/X.java
@@ -0,0 +1,9 @@
+package com.wss.example.vul4j;
+
+import java.util.List;
+
+public class X {
+
+    public String x;
+    public List<Integer> z;
+}
